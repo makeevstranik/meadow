babf83c556ccdfe6a3716ab841dcddc68b9bef6d
Makeev Evgeny <makeevstranik@gmail.com>
2018-09-06 23:51:52 +0500

    commit from linux

--- meadowlark.js
+++ meadowlark.js
@@ -1,9 +1,7 @@
 /**
  * Created by Makeev Evgeny on 17.08.18.
  */
-//const app = require('./server');
-import app from './server'
-
-app.listen(app.get('port'), () =>
-  console.log(`running express in http://localhost:${app.get('port')} press CTRL+C to quit`));
-
+const app = require('./server');
+// import app from './server';
+console.log('IN MEADOWLARK', module.parent);
+app.listen(app.get('port'), () => console.log(`running express in http://localhost:${app.get('port')} press CTRL+C to quit`));
--- server.js
+++ server.js
@@ -1,22 +1,54 @@
 /**
  * Created by Makeev Evgeny on 17.08.18.
  */
+/*
 import Express from 'express';
+import Handlebars from 'express-handlebars';
+*/
+const Express = require('express');
+const Handlebars = require('express-handlebars');
+
+// import morgan from 'morgan'; // logging lib from hexlet
+
 const app = new Express();
+const handlebars = Handlebars.create({ defaultLayout: 'main' });
+// const logger = morgan('combined'); // logging lib from hexlet
+
+console.log('handlebars: ------', handlebars.template);
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', handlebars);
 
 app.set('port', 3000);
 
-app.use((req, res) => {
+// app.use(logger); // logging lib from hexlet
+
+app.get('/', (req, res) => {
+/*
   res.type('text/plain');
+  res.status(200);
+  res.send('Meadowlark Travel');
+   // before using handlebars
+   */
+  res.render('layouts/home.handlebars');
+});
+
+app.get('/about', (req, res) => { // handle all in dir about : /about*
+  res.render('layouts/about.handlebars');
+});
+
+app.use((req, res) => { // app.use - if there is not other handler
   res.status(404);
-  res.send('404 - page not found');
+  res.render('layouts/404.handlebars');
 });
 
 app.use((err, req, res) => {
   console.error(err.stack);
-  res.type('text/plain');
   res.status(500);
-  res.send('500 server mistake');
+  res.render('layouts/500.handlebar');
 });
 
-export default app;
+
+// export default app;
+module.exports = app;
+
+
